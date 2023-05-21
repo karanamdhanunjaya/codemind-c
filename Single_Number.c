b6a800433c6a1890b9ca065a2d4b4de0e8c9dc0f
karanam Dhanunjaya <121385905+karanamdhanunjaya@users.noreply.github.com>
2023-05-21 17:48:23 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n];     for(int i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int c=0,k=0;     for(int i=0;i<n;i++)     {         c=0;         for(int j=0;j<n;j++)         {             if(i!=j)             {                 if(a[i]==a[j])                 {                     c++;                 }             }         }         if(c==1)         {             continue;         }         else         printf("%d",a[i]);         break;     } }

---
+++ Single_Number.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int c=0,k=0;
+    for(int i=0;i<n;i++)
+    {
+        c=0;
+        for(int j=0;j<n;j++)
+        {
+            if(i!=j)
+            {
+                if(a[i]==a[j])
+                {
+                    c++;
+                }
+            }
+        }
+        if(c==1)
+        {
+            continue;
+        }
+        else
+        printf("%d",a[i]);
+        break;
+    }
+}
