243896a76db9b9ca9bbd8bd8bb15d27bba9932a0
karanam Dhanunjaya <121385905+karanamdhanunjaya@users.noreply.github.com>
2023-05-24 10:35:22 +0530

    #include<stdio.h>
    int main()
    {
        int a,b;
        scanf("%d%d",&a,&b);
        int min=a<b?a:b;
        for(int i=min;i>=1;i--)
        {
            if(a%i==0&&b%i==0)
            {
                printf("%d",i);
                break;
            }
        }
    }

---
+++ GCD_or_HCF.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    int min=a<b?a:b;
+    for(int i=min;i>=1;i--)
+    {
+        if(a%i==0&&b%i==0)
+        {
+            printf("%d",i);
+            break;
+        }
+    }
+}
